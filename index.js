407bf4553a7c84541cf5cde80ff2b04eb64da86e
Irene Mutegi <irene@Ms-Echo.>
2024-09-30 12:20:50 +0300

    short message

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("swim")); // This Saturday, I want to swim!
+console.log(saturdayFun());   
+
+
+const mondayWork = function(activity) {
+  if (activity === undefined) {
+    activity = "go to the office";
+  }
+  return `This Monday, I will ${activity}.`;
+}; 
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("amazing")); // You are !!!amazing!!!
+console.log(encouragingPromptFunction());           // You are *special*!
+
